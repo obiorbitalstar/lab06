f7fd3d52bfdca7182d3315a59568d851a0c14410
AliBakerSartawi <alisartawi@live.com>
2021-03-29 09:10:47 +0300

    PORT ||

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 const cors = require('cors');
 
 // Setup
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 
